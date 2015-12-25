b71f813e827af573ade30f35598cd0140bd4eecd
webbestmaster <web.best.master@gmail.com>
2015-12-26 00:59:16 +0300

    add gulp-autoprefixer

--- server.js
+++ server.js
@@ -11,4 +11,16 @@ app.get('/api/*', function(req, res){
 // send file
 app.get('*', sendFile);
 
+// Handle 404 - implemented by helper
+/*
+app.use(function(req, res) {
+	res.send('404: Page not Found', 404);
+});
+*/
+
+// Handle 500
+app.use(function(error, req, res, next) {
+	res.send('500: Internal Server Error', 500);
+});
+
 app.listen(process.env.PORT || 3000);
